cb04f6ab067f76bc52927dd4e2700ad5e58050a5
BTepper24 <92735881+BTepper24@users.noreply.github.com>
2022-04-11 09:24:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // COmplete game function
 
-  let playScore = 0;
-  let computerScore = 0;
-  let playerChoice = "";
-  let computerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice = "";
+let computerChoice = "";
+let pCount = document.getElementById('p-count');
+let cCount = document.getElementById('c-count');
+let winnerDisplay = document.getElementById('winnerDisplay');
 //Function to grab user input and run function to evaluate choices
 
 
@@ -12,68 +15,77 @@ function computerSelect() {
   switch (choice) {
     case 0:
       computerChoice = "paper";
-        break;
+      break;
     case 1:
       computerChoice = "rock";
-        break;
+      break;
     case 2:
       computerChoice = "scissors";
+      break;
+  }
+}
+  function chooseRock() {
+    computerSelect();
+    switch (computerChoice) {
+      case "paper":
+        computerScore++;
+        winnerDisplay.textContent = 'You lose!';
         break;
+      case "rock":
+      winnerDisplay.textContent = 'You tie!';
+        break;
+      case "scissors":
+        playerScore++;
+        winnerDisplay.textContent = 'You win!';
+        break;
+    }
+    pCount.textContent = playerScore;
+    cCount.textContent = computerScore;
   }
 
-function chooseRock() {
-  computerSelect();
-  switch (computerChoice) {
-    case "paper":
-    computerScore++;
-    break;
-    case "rock":
-    break;
-    case "scissors":
-    playerScore++;
-    break;
+  function choosePaper() {
+    computerSelect();
+    switch (computerChoice) {
+      case "scissors":
+        computerScore++;
+        winnerDisplay.textContent = 'You lose!';
+        break;
+      case "paper":
+       winnerDisplay.textContent = 'You tie!';
+        break;
+      case "rock":
+        playerScore++;
+        winnerDisplay.textContent = 'You win!';
+        break;
+    }
+    pCount.textContent = playerScore;
+    cCount.textContent = computerScore;
+
   }
-  pCount.textContent = playerScore;
-  cCount.textContent = computerScore;
-}
 
-function choosePaper() {
-  computerSelect();
-  switch (computerChoice) {
-    case "paper":
-    computerScore++;
-    break;
-    case "rock":
-    break;
-    case "scissors":
-    playerScore++;
-    break;
-}
-pCount.textContent = playerScore;
-cCount.textContent = computerScore;
+  function chooseScissors() {
+    computerSelect();
+    switch (computerChoice) {
+      case "rock":
+        computerScore++;
+        winnerDisplay.textContent = 'You lose!';
+        break;
+      case "scissors":
+      winnerDisplay.textContent = 'You tie!';
+        break;
+      case "paper":
+        playerScore++;
+        winnerDisplay.textContent = 'You win!';
+        break;
+    }
+    pCount.textContent = playerScore;
+    cCount.textContent = computerScore;
 
-}
+  }
 
-function chooseScissors() {
-  computerSelect();
-  switch (computerChoice) {
-    case "paper":
-    computerScore++;
-    break;
-    case "rock":
-    break;
-    case "scissors":
-    playerScore++;
-    break;
-}
-pCount.textContent = playerScore;
-cCount.textContent = computerScore;
-
-}
-
-function reset() {
-  computerScore = 0;
-  playerScore = 0;
-  pCount.textContent = playerScore;
-  cCount.textContent = computerScore;
-}
+  function reset() {
+    computerScore = 0;
+    playerScore = 0;
+    pCount.textContent = playerScore;
+    cCount.textContent = computerScore;
+  }
